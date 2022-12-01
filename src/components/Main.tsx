54002a2a22b98131aaab73c75438332b6811db21
Jordan Khalil <jordankhalil0@gmail.com>
2022-12-01 16:41:41 -0500

    Jordan's update part2

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,32 +1,41 @@
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import Article from "../models/Article";
 import { getAllArticles } from "../services/spaceFlightApiService";
 import "./Main.css";
+
+const randomArticles = (articles: Article[]) => {
+  let numOfArticles: Article[] = [];
+  for (let i = 0; i < 10; i++) {
+    let randomIndex: number = Math.floor(Math.random() * articles.length);
+    let foundIndex: number = numOfArticles.findIndex((article) => {
+      return article.id === articles[randomIndex].id;
+    });
+    if (foundIndex === -1) {
+      numOfArticles.push(articles[randomIndex]);
+    } else {
+      i--;
+    }
+  }
+  return numOfArticles;
+};
 
 const Main = () => {
   const [articles, setArticles] = useState<Article[]>([]);
   useEffect(() => {
     getAllArticles().then((res) => {
-      setArticles(res);
+      setArticles(randomArticles(res));
     });
   }, []);
 
-  const randomArticles = (articles: Article[]) => {
-    let numOfArticles: Article[] = [];
-    for (let i = 0; i < 10; i++) {
-      numOfArticles.push(articles[Math.floor(Math.random() * articles.length)]);
-    }
-    return numOfArticles;
-  };
-
-  const numOfArticles: Article[] = randomArticles(articles);
-
   return (
     <div className="Main">
-      Space
+      <Link to="/typing-page">
+        <button>Move to typing page</button>
+      </Link>
       <div>
         {articles.length > 0 &&
-          numOfArticles.map((article) => (
+          articles.map((article) => (
             <div key={`${article.id}${Math.floor(Math.random() * 1000)}`}>
               <p>{article.title}</p>
               <img src={article.imageUrl} alt={article.title} />
