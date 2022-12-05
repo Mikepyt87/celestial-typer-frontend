33e1f92736fd3f049f41b03f6a7a9a1275a7c8b7
JoshHerty <joshuaherty7@gmail.com>
2022-12-05 11:47:41 -0500

    figuring out undefined issue

--- src/components/TypingGame/hooks/useEngine.ts
+++ src/components/TypingGame/hooks/useEngine.ts
@@ -1,4 +1,6 @@
 import { useCallback, useEffect, useState } from "react";
+import Article from "../../../models/Article";
+import { getAllArticles } from "../../../services/spaceFlightApiService";
 import { countErrors, debug } from "../utils/helpers";
 import useCountdown from "./useCountdown";
 import useTypings from "./useTypings";
@@ -6,14 +8,16 @@ import useWords from "./useWords";
 
 export type State = "start" | "run" | "finish";
 
-const NUMBER_OF_WORDS = 12;
-const COUNTDOWN_SECONDS = 60;
+// const NUMBER_OF_WORDS = 12;
+const COUNTDOWN_SECONDS = 3;
 
-const useEngine = () => {
+const useEngine = (articles: Article[]) => {
   const [state, setState] = useState<State>("start");
   const { timeLeft, startCountdown, resetCountdown } =
     useCountdown(COUNTDOWN_SECONDS);
-  const { words, updateWords } = useWords(NUMBER_OF_WORDS);
+
+  const { words, updateWords } = useWords(articles, 0);
+
   const { cursor, typed, clearTyped, totalTyped, resetTotalTyped } = useTypings(
     state !== "finish"
   );
@@ -22,15 +26,15 @@ const useEngine = () => {
   const isStarting = state === "start" && cursor > 0;
   const areWordsFinished = cursor === words.length;
 
-  const restart = useCallback(() => {
-    debug("restarting...");
-    resetCountdown();
-    resetTotalTyped();
-    setState("start");
-    setErrors(0);
-    updateWords();
-    clearTyped();
-  }, [clearTyped, updateWords, resetCountdown, resetTotalTyped]);
+  // const restart = useCallback(() => {
+  //   debug("restarting...");
+  //   resetCountdown();
+  //   resetTotalTyped();
+  //   setState("start");
+  //   setErrors(0);
+  //   updateWords();
+  //   clearTyped();
+  // }, [clearTyped, updateWords, resetCountdown, resetTotalTyped]);
 
   const sumErrors = useCallback(() => {
     debug(`cursor: ${cursor} - words.length: ${words.length}`);
@@ -68,7 +72,15 @@ const useEngine = () => {
     }
   }, [clearTyped, areWordsFinished, updateWords, sumErrors]);
 
-  return { state, words, typed, errors, restart, timeLeft, totalTyped };
+  return {
+    state,
+    words,
+    typed,
+    errors,
+    // restart,
+    timeLeft,
+    totalTyped,
+  };
 };
 
 export default useEngine;
