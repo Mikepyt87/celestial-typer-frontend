6a308ee4d565606c87403d73bca9b0c523c73331
Jordan Khalil <jordankhalil0@gmail.com>
2022-12-05 16:54:17 -0500

    Jordan's Update part 2

--- src/components/TypingGame/ResultsPage.tsx
+++ src/components/TypingGame/ResultsPage.tsx
@@ -1,12 +1,28 @@
 import { useContext } from "react";
+import { Link } from "react-router-dom";
 import ResultsContext from "../../context/ResultsContext";
+import RestartButton from "./RestartButton";
+import Results from "./Results";
 import "./ResultsPage.css";
 
 const ResultsPage = () => {
   const { results } = useContext(ResultsContext);
   console.log(results);
 
-  return <div className="ResultsPage">ResultsPage works</div>;
+  return (
+    <div className="ResultsPage">
+      ResultsPage works
+      <Results
+        state={"finish"}
+        errors={results!.errors}
+        accuracyPercentage={results!.accuracyPercentage}
+        total={results!.total}
+      />
+      <Link to="/typing-page">
+        <button>Im a button</button>
+      </Link>
+    </div>
+  );
 };
 
 export default ResultsPage;
