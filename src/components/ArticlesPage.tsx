38641e814958c51a5a33a27a242ea850cfe5b010
Mikepyt87 <81245551+Mikepyt87@users.noreply.github.com>
2022-12-12 13:37:03 -0500

    articlesPage

--- src/components/ArticlesPage.tsx
+++ src/components/ArticlesPage.tsx
@@ -1,11 +1,41 @@
-import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import replacementImg from "../assets/nasa_logo.jpg";
+import { Link, useParams } from "react-router-dom";
+import Article from "../models/Article";
+import { getSpecificArticle } from "../services/spaceFlightApiService";
 import "./ArticlesPage.css";
+import Header from "./Header";
 
 const ArticlesPage = () => {
+  const articleId: string | undefined = useParams().articleId;
+
+  const [article, setArticle] = useState<Article[]>();
+
+  useEffect(() => {
+    getSpecificArticle(+articleId!).then((res) => setArticle(res));
+  }, []);
+
   return (
-    <Link to="/articlesPage">
-      <div className="ArticlesPage">ArticlesPage works</div>
-    </Link>
+    <>
+      <Header />
+      <main className="ArticlesPage">
+        {article?.map((article) => (
+          <section key={`${article.id}_${article.publishedAt}`}>
+            <h1>{article.title}</h1>
+            <p>{article.summary}</p>
+            <img
+              src={article.imageUrl}
+              alt={article.title}
+              className="article-image"
+              // if image is not found, then load replacementImg
+              onError={({ currentTarget }) => {
+                currentTarget.src = replacementImg;
+              }}
+            />
+          </section>
+        ))}
+      </main>
+    </>
   );
 };
 
