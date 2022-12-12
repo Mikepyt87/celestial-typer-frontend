15061dcb2819ee5eeaaa21967041f4d83db6571b
Mikepyt87 <81245551+Mikepyt87@users.noreply.github.com>
2022-12-11 22:58:32 -0500

    uno mas

--- src/components/custom hooks/useWindowDimensions.ts
+++ src/components/custom hooks/useWindowDimensions.ts
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 
+//* returns the current width and height of user's window
 function getWindowDimensions() {
   const { innerWidth: width, innerHeight: height } = window;
   return {
@@ -9,15 +10,18 @@ function getWindowDimensions() {
 }
 
 export default function useWindowDimensions() {
+  //* useState hook
   const [windowDimensions, setWindowDimensions] = useState(
     getWindowDimensions()
   );
 
+  //* useEffect hook
   useEffect(() => {
     function handleResize() {
       setWindowDimensions(getWindowDimensions());
     }
 
+    //* updates state when window is resized
     window.addEventListener("resize", handleResize);
     return () => window.removeEventListener("resize", handleResize);
   }, []);
